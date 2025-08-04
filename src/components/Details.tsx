4683bdd4cb390612505c4f14afa3413614bcd3c0
Rafaela Zanezi <zar2ca@bosch.com>
2025-08-04 16:55:57 -0300

    feat: criada página de detailhes da obra

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,11 +1,12 @@
 import AppBar from "@mui/material/AppBar"
 import Dialog from "@mui/material/Dialog"
+import Link from "@mui/material/Link"
 import Slide from "@mui/material/Slide"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import { ReactNode } from "react"
-import { CollectionObject } from "../models/CollectionObject"
 import "../components/Details.css"
+import { CollectionObject } from "../models/CollectionObject"
 
 interface DetailsProps {
     isOpen: boolean;
@@ -32,9 +33,33 @@ const Details = ({ isOpen, onClose, children, details }: DetailsProps) => {
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <img className="banner" src={details ? details.primaryImage : ""} alt={details ? details.title : "Details"} />
-            
-        
+
+            <div className="container-details">
+                <img className="banner" src={
+                    details?.primaryImage || details?.primaryImageSmall || "https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small_2x/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                } alt={details?.title ?? "Details"} />
+
+            </div>
+                <div className="details">
+                    <Typography variant="body1">
+                        {`${details?.title || "No title available"}`}
+                    </Typography>
+                    <Typography variant="body1">
+                        {`${details?.artistDisplayName || "Unknown artist"}`}
+                    </Typography>
+                    <Typography variant="body1">
+                        {`(${details?.objectBeginDate} - ${details?.objectEndDate})`}
+                    </Typography>
+                    <Typography variant="body1">
+                        {`${details?.medium}`}
+                    </Typography>
+                    <Typography variant="body1">
+                        {`Department: ${details?.department || "Unknown department"}`}
+                    </Typography>
+                    <Link href={details?.objectWikidata_URL} color="inherit" target="_blank">
+                        View more
+                    </Link>
+                </div>
         </Dialog>
     )
 }
--- src/models/CollectionObject.ts
+++ src/models/CollectionObject.ts
@@ -7,6 +7,11 @@ export interface CollectionObject {
     title: string;
     artistDisplayName: string;
     artistDisplayBio: string;
+    objectBeginDate: number;
+    objectEndDate: number;
+    medium: string;
+    department: string;
+    objectWikidata_URL: string;
 }
 
 export interface Constituents {
@@ -17,98 +22,3 @@ export interface Constituents {
     constituentWikidata_URL: string;
     gender: string;
 }
-
-// imagem, título, artista, data, técnica, departamento, link para o site oficial
-/**
- {
-    "objectID": 51868,
-    "isHighlight": false,
-    "accessionNumber": "1984.492.3",
-    "accessionYear": "1984",
-    "isPublicDomain": true,
-    "primaryImage": "https://images.metmuseum.org/CRDImages/as/original/DP154393.jpg",
-    "primaryImageSmall": "https://images.metmuseum.org/CRDImages/as/web-large/DP154393.jpg",
-    "additionalImages": [],
-    "constituents": [
-        {
-            "constituentID": 37502,
-            "role": "Artist",
-            "name": "Unidentified artist",
-            "constituentULAN_URL": "",
-            "constituentWikidata_URL": "",
-            "gender": ""
-        }
-    ],
-    "department": "Asian Art",
-    "objectName": "Album leaf",
-    "title": "Painting",
-    "culture": "China",
-    "period": "Qing dynasty (1644–1911)",
-    "dynasty": "",
-    "reign": "",
-    "portfolio": "",
-    "artistRole": "Artist",
-    "artistPrefix": "",
-    "artistDisplayName": "Unidentified artist",
-    "artistDisplayBio": "",
-    "artistSuffix": "",
-    "artistAlphaSort": "Unidentified artist",
-    "artistNationality": "",
-    "artistBeginDate": "",
-    "artistEndDate": "",
-    "artistGender": "",
-    "artistWikidata_URL": "",
-    "artistULAN_URL": "",
-    "objectDate": "20th century",
-    "objectBeginDate": 1900,
-    "objectEndDate": 1911,
-    "medium": "Leaf from an album; ink on paper",
-    "dimensions": "H. 9 11/16 in. (24.6 cm); W. 12 in. (30.5 cm)",
-    "measurements": [
-        {
-            "elementName": "Overall",
-            "elementDescription": null,
-            "elementMeasurements": {
-                "Height": 24.6,
-                "Width": 30.5
-            }
-        },
-        {
-            "elementName": "Other",
-            "elementDescription": "in mat",
-            "elementMeasurements": {
-                "Height": 31.1,
-                "Width": 38.1
-            }
-        }
-    ],
-    "creditLine": "Gift of Fritzi and Murray Sanders, 1984",
-    "geographyType": "",
-    "city": "",
-    "state": "",
-    "county": "",
-    "country": "",
-    "region": "",
-    "subregion": "",
-    "locale": "",
-    "locus": "",
-    "excavation": "",
-    "river": "",
-    "classification": "Paintings",
-    "rightsAndReproduction": "",
-    "linkResource": "",
-    "metadataDate": "2025-03-06T04:54:30Z",
-    "repository": "Metropolitan Museum of Art, New York, NY",
-    "objectURL": "https://www.metmuseum.org/art/collection/search/51868",
-    "tags": [
-        {
-            "term": "Men",
-            "AAT_URL": "http://vocab.getty.edu/page/aat/300025928",
-            "Wikidata_URL": "https://www.wikidata.org/wiki/Q8441"
-        }
-    ],
-    "objectWikidata_URL": "https://www.wikidata.org/wiki/Q78762496",
-    "isTimelineWork": false,
-    "GalleryNumber": ""
-}
- */
